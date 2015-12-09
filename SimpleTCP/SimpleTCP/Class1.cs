e32818902c0a326bbfd6e58bb7c06dbf34dd6c6b
mcmadmac11 <mcmanusdbm@gmail.com>
2015-12-09 09:47:56 -0600

    initial commit

---
+++ SimpleTCP/SimpleTCP/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTCP
+{
+    public class IPHeader<T>
+    {
+        public T CreateIPHeader()
+        {
+            throw new NotImplementedException();    
+        }
+
+        private bool isTCPPacket;
+        {
+            if (true);
+            {
+                    
+            }
+	
+
+	                    
+        public bool IsTCPPacket
+        {
+            get
+            {
+                return isTCPPacket;
+            }
+
+            set
+            {
+                this.isTCPPacket = value;
+            }
+        }
+    }
+}
