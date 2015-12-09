c9181ac279ed2b34b89fb60c7aba72340f929ba7
mcmadmac11 <mcmanusdbm@gmail.com>
2015-12-09 12:19:10 -0600

    created binary to object and object to binary converters

---
+++ SimpleTCP/SimpleTCP/TCPPacket.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace SimpleTCP
+{
+    class TCPPacket<T>
+    {
+        public byte[] ObjectToByteArray(Object obj)
+        {
+            if(obj == null)
+            return null;
+            BinaryFormatter binFormat = new BinaryFormatter();
+            MemoryStream memStrm = new MemoryStream();
+            binFormat.Serialize(memStrm, obj); 
+            return memStrm.ToArray();             
+        }
+
+        public object ByteToArray(byte[] arrBytes)
+        {
+            MemoryStream memoryStream = new MemoryStream();
+            BinaryFormatter binaryForm = new BinaryFormatter();
+            memoryStream.Write(arrBytes, 0, arrBytes.Length);
+            memoryStream.Seek(0, SeekOrigin.Begin);
+            Object obj = (Object) binaryForm.Deserialize(memoryStream);
+            return obj;
+        }
+    }
+}
